5633ff38b6ddf6fc83155cefd4f347b9b4f60247
sandracarmena <119263403+sandracarmena@users.noreply.github.com>
2023-02-14 19:26:59 +0100

    removing dist in gitgnore

---
+++ dist/dom.bundle.js
@@ -0,0 +1,66 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./JS/dom.js":
+/*!*******************!*\
+  !*** ./JS/dom.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Venture\": () => (/* binding */ Venture),\n/* harmony export */   \"businessFreedom\": () => (/* binding */ businessFreedom),\n/* harmony export */   \"button\": () => (/* binding */ button),\n/* harmony export */   \"cityDescription\": () => (/* binding */ cityDescription),\n/* harmony export */   \"comute\": () => (/* binding */ comute),\n/* harmony export */   \"culture\": () => (/* binding */ culture),\n/* harmony export */   \"economy\": () => (/* binding */ economy),\n/* harmony export */   \"education\": () => (/* binding */ education),\n/* harmony export */   \"environmentalQuality\": () => (/* binding */ environmentalQuality),\n/* harmony export */   \"erreur\": () => (/* binding */ erreur),\n/* harmony export */   \"healthcare\": () => (/* binding */ healthcare),\n/* harmony export */   \"housing\": () => (/* binding */ housing),\n/* harmony export */   \"input\": () => (/* binding */ input),\n/* harmony export */   \"internet\": () => (/* binding */ internet),\n/* harmony export */   \"livingCost\": () => (/* binding */ livingCost),\n/* harmony export */   \"outdoors\": () => (/* binding */ outdoors),\n/* harmony export */   \"overallScore\": () => (/* binding */ overallScore),\n/* harmony export */   \"paragraph\": () => (/* binding */ paragraph),\n/* harmony export */   \"safety\": () => (/* binding */ safety),\n/* harmony export */   \"startup\": () => (/* binding */ startup),\n/* harmony export */   \"taxation\": () => (/* binding */ taxation),\n/* harmony export */   \"tolerance\": () => (/* binding */ tolerance),\n/* harmony export */   \"travel\": () => (/* binding */ travel)\n/* harmony export */ });\nconst input = document.querySelector(\"input\");\nconst paragraph = document.getElementById(\"city-text\");\nconst button = document.querySelector(\"button\");\nconst cityDescription = document.getElementById(\"city-description\");\nconst housing = document.getElementById(\"housing\");\nconst travel = document.getElementById(\"travel\");\nconst healthcare = document.getElementById(\"healthcare\");\nconst education = document.getElementById(\"education\");\nconst livingCost = document.getElementById(\"living-cost\");\nconst startup = document.getElementById(\"startup\");\nconst Venture = document.getElementById(\"venture\");\nconst comute = document.getElementById(\"comute\");\nconst businessFreedom = document.getElementById(\"business-freedom\");\nconst economy = document.getElementById(\"economy\");\nconst safety = document.getElementById(\"safety\");\nconst taxation = document.getElementById(\"taxation\");\nconst internet = document.getElementById(\"internet\");\nconst culture = document.getElementById(\"culture\");\nconst tolerance = document.getElementById(\"tolerance\");\nconst outdoors = document.getElementById(\"outdoors\");\nconst environmentalQuality = document.getElementById(\n  \"environmental-quality\"\n);\nconst overallScore = document.getElementById(\"overall\");\nconst erreur = document.getElementById(\"erreur\");\n\n\n//# sourceURL=webpack://webpack-demo/./JS/dom.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./JS/dom.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
